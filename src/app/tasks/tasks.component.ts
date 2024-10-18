440791c55f16fca4b02bcc67030f98faa0357170
Rob Blake <rob.bl8ke@outlook.com>
2024-10-18 12:41:49 +0200

    Inject task using inject() function with service token

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
-import { NewTask } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
@@ -23,20 +22,11 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-    this.tasksService.removeTask(id);
-  }
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(task: NewTask) {
-    this.tasksService.addTask(task, this.userId);
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
