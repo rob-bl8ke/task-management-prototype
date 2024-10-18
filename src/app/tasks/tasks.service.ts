18e022e6cfb5dad172502a6f6d995cea84e7d14d
Rob Blake <rob.bl8ke@outlook.com>
2024-10-18 12:48:28 +0200

    Local storage: Use local storage to persist tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,6 +31,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId)
   }
@@ -43,9 +50,15 @@ export class TasksService {
       summary: task.summary, 
       dueDate: task.date 
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
